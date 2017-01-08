b3eec2fc0cb62d734e6e765f039ac744ceae6688
Aldo Sanchez <Aldo Sanchez>
2017-01-07 18:05:31 -0600

    :construction:momentjs math done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,9 @@
 console.log('hello world');
 console.log(moment('20111031', 'YYYYMMDD').fromNow());
+var firstTrainTime = '0700';
+var trainFrequency = 30;
+var testTime = '1010';
+var timeDifference = moment().diff(moment(firstTrainTime,'HHmm'),'minutes');
 
-firstTrainTime;
-trainFrequency;
+var remainder = parseInt(timeDifference)%trainFrequency
+var nextTrain = Math.abs(remainder-trainFrequency);
